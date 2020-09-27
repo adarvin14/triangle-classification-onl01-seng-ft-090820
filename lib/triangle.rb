4fcd55252c7c35765f1ede020900bc8aa668134a
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 21:03:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,16 +14,15 @@ class Triangle
     elsif side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b
       raise TriangleError
     elsif
-      side_a == 0 && side_b == 0 & side_c = 0
+      side_a = 0 && side_b = 0 & side_c = 0
       raise TriangleError
-    elsif
       side_a = side_b && side_b = side_c
       :equilateral
     elsif
-      side_a == side_b || side_b == side_c || side_a == side_c
+      side_a = side_b || side_b = side_c || side_a = side_c
       :isosceles
-    elsif
-      side_a != side_b && side_b != side_c && side_a != side_c
+    else
+      side_a != side_b && side_a != side_c && side_b != side_c
       :scalene
     end
   end
