48dde5caa9935aca18940e1f3ae2d049f0ab78d4
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 20:57:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,9 +19,6 @@ class Triangle
     elsif
       side_a == side_b = side_c
       :equilateral
-    elsif
-      side_a == side_b || side_b == side_c || side_a == side_c
-      :isosceles
     else
       :scalene
     end
