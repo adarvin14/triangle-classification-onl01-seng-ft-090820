f31a2295006c091639db6ce6532fbe922e7812e5
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 21:04:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     elsif side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b
       raise TriangleError
     elsif
-      side_a == 0 && side_b == 0 & side_c == 0
+      side_a == 0 && side_b == 0 & side_c = 0
       raise TriangleError
     elsif
       side_a = side_b = side_c
