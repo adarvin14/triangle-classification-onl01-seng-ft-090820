45b0ddd2aab48b583bdce7e20c1ed23715934224
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 21:15:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,9 @@ class Triangle
   end
   
   def kind
-    if side_a <= 0 || side_b <= 0 || side_c <= 0
+    if (side_a <= 0 || side_b <= 0 || side_c <= 0) || (side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b)
       raise TriangleError
-    elsif side_a + side_b <= side_c || side_b + side_c <= side_a || side_a + side_c <= side_b
-      raise TriangleError
-    elsif
-      side_a == 0 && side_b == 0 & side_c = 0
+    elsif side_a == b && side_2 == 0 && side_c == 0
       raise TriangleError
     elsif
       side_a == side_b && side_b == side_c
